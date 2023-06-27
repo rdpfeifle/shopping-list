a87a94d0b1c5a8cc6713e5b17460648ef78aad9c
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2023-06-26 21:34:20 -0600

    Add new functionality and fix bug

--- index.js
+++ index.js
@@ -5,22 +5,42 @@ import {
   ref,
   push,
   onValue,
+  remove,
 } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js";
 
 const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-const shoppingItemsInDB = ref(database, "shopping-items");
+const db = getDatabase(app);
+const shoppingItemsInDB = ref(db, "shopping-items");
 
 const inputField = document.getElementById("input-field");
 const addToCart = document.getElementById("add-button");
 const shoppingListEl = document.getElementById("shopping-list");
 
+/* Clear input field after adding to cart */
 const clearInputField = () => {
   inputField.value = "";
 };
 
-const addNewItem = (item) => {
-  shoppingListEl.innerHTML += `<li>${item}</li>`;
+/* Delete item from the shopping list */
+const deleteItem = (itemToDelete) => {
+  let locationOfItemInDB = ref(db, `shopping-items/${itemToDelete}`);
+  remove(locationOfItemInDB);
+};
+
+/* Add item from the shopping list */
+const addNewItem = (newItem) => {
+  let itemID = newItem[0];
+  let itemVal = newItem[1];
+
+  let newEl = document.createElement("li");
+
+  newEl.textContent = itemVal;
+
+  newEl.addEventListener("dblclick", () => {
+    deleteItem(itemID);
+  });
+
+  shoppingListEl.append(newEl);
 };
 
 const clearShoppingList = () => {
@@ -37,11 +57,21 @@ addToCart.addEventListener("click", () => {
 });
 
 onValue(shoppingItemsInDB, (snapshot) => {
-  let itemsArray = Object.values(snapshot.val());
+  if (snapshot.exists()) {
+    let itemsArray = Object.entries(snapshot.val());
 
-  clearShoppingList();
+    clearShoppingList();
 
-  for (let item in itemsArray) {
-    addNewItem(itemsArray[item]);
+    for (let item in itemsArray) {
+      let currentItem = itemsArray[item];
+
+      /* let currentItemID = currentItem[0]; // key
+      let currentItemValue = currentItem[1]; // value
+      */
+
+      addNewItem(currentItem);
+    }
+  } else {
+    shoppingListEl.innerHTML = "No items here... yet";
   }
 });
